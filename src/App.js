9e2ee9e9bf03a09991497f0d533ab2bf9bdca98a
nikhil <nikhilatoliya12@gmail.com>
2021-07-09 20:38:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
 import LogoutButton from './components/Logout';
 import Home from './components/Home';
-import GetButton from "./components/GetButton"
+// import GetButton from "./components/GetButton"
 import {useAuth0} from "@auth0/auth0-react";
 
 
