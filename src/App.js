847465ba29c55fbb1f9ce6acbaec5efc3bc82964
nikhil <nikhilatoliya12@gmail.com>
2021-07-13 14:54:34 +0530

    deploy error resolved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
-import LogoutButton from './components/Logout';
-import Home from './components/Home';
+// import LogoutButton from './components/Logout';
+// import Home from './components/Home';
 // import GetButton from "./components/GetButton"
 import {useAuth0} from "@auth0/auth0-react";
 import Hero from './components/Hero';
